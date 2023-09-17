d466ab9ca569619028973489dd800549fbc481ee
Myrna Prieto <myrnapt96@gmail.com>
2023-09-17 22:14:54 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
+import { HeaderComponent } from './pages/home/header/header.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PopupComponent } from './shared/popup/popup.component';
